d500be8c81f07aba4c26d11006ac8a316aad330b
Luiz Carlos Favaro <luiz.favaro@finansystech.com>
2023-12-12 18:15:05 -0300

    starship changes and codium alias on pws

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,5 +2,6 @@ $ENV:SPACESHIP_PROMPT_ASYNC =$false
 
 set-alias -name pn -value pnpm
 set-alias -name lg -value lazygit
+set-alias -name code -value codium
 
 Invoke-Expression (&starship init powershell)
