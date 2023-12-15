0be424199d60e27c0a14199735187d22882c8821
Luiz Carlos Favaro <luiz.favaro@finansystech.com>
2023-12-15 15:36:22 -0300

    added some alias

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,5 +3,8 @@ $ENV:SPACESHIP_PROMPT_ASYNC =$false
 set-alias -name pn -value pnpm
 set-alias -name lg -value lazygit
 set-alias -name code -value codium
+set-alias -name dc -value docker-compose
+set-alias -name d -value docker
+set-alias -name cat -value bat
 
 Invoke-Expression (&starship init powershell)
